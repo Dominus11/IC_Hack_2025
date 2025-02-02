ebf5ab5d734d598144f839c173cd0a029f36b61b
Sarath Roshin-Saran <sroshinsaran@gmail.com>
2025-02-02 11:18:01 +0000

    Final Testing (?)

--- runYolo.zsh
+++ runYolo.zsh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 
 cd "/Users/sarath/Downloads/Projects/IC_Hack_2025/yolov9"
-python detect.py --weights gelan-c.pt --conf 0.5 --source 1 --device mps
+python detect.py --weights gelan-c.pt --conf 0.5 --source 2 --device mps
